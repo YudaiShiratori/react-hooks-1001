63e5561cc686f69f824a097efbf852363fc63ab3
YudaiShiratori <toritorikebabu1023x@gmail.com>
2020-03-18 10:56:08 +0900

    my first React Hook withuseState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+  const [count, setCount] = useState(0)
+
+  const increment = () => setCount(count + 1)
+  const decrement = () => setCount(count - 1)
+
+  const reset = () => setCount(0)
+  const duplicate = () => setCount(count * 2)
+  const devide3 = () => setCount(previousCount => 
+    previousCount % 3 === 0 ? previousCount / 3 : previousCount
+  )
+
   return (
-    <div>
-      This is Template for React App
-    </div>
+    <>
+      <div>
+        count: {count}
+      </div>
+      <div>
+        <button onClick={increment}>+1</button>
+        <button onClick={decrement}>-1</button>
+      </div>
+      <div>
+        <button onClick={reset}>reset</button>
+        <button onClick={duplicate}>duplicate</button>
+        <button　onClick={devide3}>ナベアツ</button>
+      </div>
+    </>
   )
 }
 
