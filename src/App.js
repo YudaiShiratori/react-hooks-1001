5c3ea16fe1e5878ac84a1c070f67e066883e6429
YudaiShiratori <toritorikebabu1023x@gmail.com>
2020-03-18 14:23:14 +0900

    implement component using multiple states

--- src/App.js
+++ src/App.js
@@ -1,33 +1,29 @@
 import React, { useState } from 'react'
 
-const App = () => {
-  const [count, setCount] = useState(0)
+const App = props => {
+  const [name, setName] = useState(props.name)
+  const [price, setPrice] = useState(props.price)
 
-  const increment = () => setCount(count + 1)
-  const decrement = () => setCount(count - 1)
-
-  const reset = () => setCount(0)
-  const duplicate = () => setCount(count * 2)
-  const devide3 = () => setCount(previousCount => 
-    previousCount % 3 === 0 ? previousCount / 3 : previousCount
-  )
-
+  const reset = () => {
+    setPrice(props.price)
+    setName(props.name)
+  }
   return (
     <>
-      <div>
-        count: {count}
-      </div>
-      <div>
-        <button onClick={increment}>+1</button>
-        <button onClick={decrement}>-1</button>
-      </div>
-      <div>
-        <button onClick={reset}>reset</button>
-        <button onClick={duplicate}>duplicate</button>
-        <button　onClick={devide3}>ナベアツ</button>
-      </div>
+      <p>
+        現在の{name}は{price}です
+      </p>
+      <button onClick={( )=> setPrice(price + 1 )}>+1</button>
+      <button onClick={() => setPrice(price - 1)}>-1</button>
+      <button onClick={reset}>reset</button>
+      <input value={name} onChange={e => setName(e.target.value)}/>
     </>
   )
+}
+
+App.defaultProps = {
+  name: 'aa',
+  price: 1000
 }
 
 export default App
